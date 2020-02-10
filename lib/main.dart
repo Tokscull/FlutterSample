81aca4720ab7d670699c2b5f1db800db3ebcf3e7
Yahor Kirylenka <YahorKirylenka@ibagroup.eu>
2020-02-10 14:40:53 +0300

    feature(worldTimeApp): create simple world time app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/pajes/choose_location.dart';
+import 'package:flutter_sample/pajes/home.dart';
+import 'package:flutter_sample/pajes/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello'),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 
 ));
 
